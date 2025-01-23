99b26f37ce2c61b4ad439c11f9ac0ffe429486e7
Mohammad Mehdi Diaromidi <85248048+mehdi-diaromidi@users.noreply.github.com>
2025-01-23 18:53:36 +0330

    just start and creating the  base of project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require 'vendor/autoload.php';
+
+use Symfony\Component\DomCrawler\Crawler;
+
+//url
+
+// get the data from url
+
+// loop from data
+
+// search the values that we need
+
+// echo data to the screen
